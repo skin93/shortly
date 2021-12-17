248f1cdd6e6038165109eb901c6c5c4686b50a78
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-12-17 23:05:49 +0100

    Add footer

--- src/components/Layout/Footer/Footer.styled.js
+++ src/components/Layout/Footer/Footer.styled.js
@@ -1,6 +1,87 @@
+import { Container } from "GlobalStyle";
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   width: 100%;
-  background-color: transparent;
+  padding-block: 60px;
+  background-color: ${({ theme }) => theme.colors.neutral.veryDarkViolet};
 `;
+
+export const StyledContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: start;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    grid-template-columns: 35% 65%;
+  }
+`;
+
+export const FooterHeading = styled.h3`
+  color: #fff;
+  font-size: 1.7rem;
+  text-align: center;
+  margin-bottom: 10px;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    text-align: left;
+    font-size: 3rem;
+    margin-bottom: 0;
+  }
+`;
+
+export const FooterContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+export const FooterLinkItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    text-align: left;
+  }
+`;
+
+export const FoooterLinkTitle = styled.h4`
+  color: #fff;
+  margin-top: 50px;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    margin-top: 0;
+  }
+`;
+
+export const FooterLink = styled.a`
+  color: ${({ theme }) => theme.colors.neutral.gray};
+  margin-block: 10px;
+  cursor: pointer;
+
+  &:first-of-type {
+    margin-top: 30px;
+  }
+`;
+
+export const FooterMediaWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 30px;
+  margin-top: 60px;
+
+  @media ${({ theme }) => theme.device.desktop} {
+    margin-top: 0;
+  }
+`;
+
+export const FooterMediaLink = styled.a`
+  cursor: pointer;
+`;
+
+export const Icon = styled.img``;
--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,10 +1,62 @@
-import { Container } from "GlobalStyle";
-import { StyledFooter } from "./Footer.styled";
+import {
+  StyledContainer,
+  FooterContent,
+  FoooterLinkTitle,
+  FooterLink,
+  FooterLinkItem,
+  FooterMediaWrapper,
+  FooterMediaLink,
+  StyledFooter,
+  FooterHeading,
+  Icon,
+} from "./Footer.styled";
+
+import FBIcon from "assets/icon-facebook.svg";
+import TwitterIcon from "assets/icon-twitter.svg";
+import PinterestIcon from "assets/icon-pinterest.svg";
+import InstagramIcon from "assets/icon-instagram.svg";
 
 export default function Footer() {
   return (
     <StyledFooter>
-      <Container>The Footer</Container>
+      <StyledContainer>
+        <FooterHeading>Shortly</FooterHeading>
+        <FooterContent>
+          <FooterLinkItem>
+            <FoooterLinkTitle>Features</FoooterLinkTitle>
+            <FooterLink>Link Shortening</FooterLink>
+            <FooterLink>Branded Links</FooterLink>
+            <FooterLink>Analytics</FooterLink>
+          </FooterLinkItem>
+          <FooterLinkItem>
+            <FoooterLinkTitle>Resources</FoooterLinkTitle>
+            <FooterLink>Blog</FooterLink>
+            <FooterLink>Developers</FooterLink>
+            <FooterLink>Support</FooterLink>
+          </FooterLinkItem>
+          <FooterLinkItem>
+            <FoooterLinkTitle>Company</FoooterLinkTitle>
+            <FooterLink>About</FooterLink>
+            <FooterLink>Our Team</FooterLink>
+            <FooterLink>Careers</FooterLink>
+            <FooterLink>Contact</FooterLink>
+          </FooterLinkItem>
+          <FooterMediaWrapper>
+            <FooterMediaLink>
+              <Icon src={FBIcon} />
+            </FooterMediaLink>
+            <FooterMediaLink>
+              <Icon src={TwitterIcon} />
+            </FooterMediaLink>
+            <FooterMediaLink>
+              <Icon src={PinterestIcon} />
+            </FooterMediaLink>
+            <FooterMediaLink>
+              <Icon src={InstagramIcon} />
+            </FooterMediaLink>
+          </FooterMediaWrapper>
+        </FooterContent>
+      </StyledContainer>
     </StyledFooter>
   );
 }
